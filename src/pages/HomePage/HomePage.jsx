16b9ce6a7c0f9e20112134f98143481cdeb17c4f
Mohammed-ziou <madiziou@gmail.com>
2021-11-04 17:53:58 +0200

    finshed styling the homepage section

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Dirctory from "../../components/dirctory/Dirctory";
+import "./HomePage.scss";
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="homepage">
+          <Dirctory />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
