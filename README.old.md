f75680f9c6bd99d5048aab167e8dc6b68808d24d
Mohammed-ziou <madiziou@gmail.com>
2021-11-04 12:48:14 +0200

    just started

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# e-com
+a react tutorial
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./HomePage.scss";
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="homepage">
+          <div className="directory-menu">
+            <div className="menu-item">
+              <div className="content">
+                <h1 className="title">Hats</h1>
+                <span className="subtitle">buy hats</span>
+              </div>
+            </div>
+            <div className="menu-item">
+              <div className="content">
+                <h1 className="title">Hats</h1>
+                <span className="subtitle">buy hats</span>
+              </div>
+            </div>
+            <div className="menu-item">
+              <div className="content">
+                <h1 className="title">Hats</h1>
+                <span className="subtitle">buy hats</span>
+              </div>
+            </div>
+            <div className="menu-item">
+              <div className="content">
+                <h1 className="title">Hats</h1>
+                <span className="subtitle">buy hats</span>
+              </div>
+            </div>
+            <div className="menu-item">
+              <div className="content">
+                <h1 className="title">Hats</h1>
+                <span className="subtitle">buy hats</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
